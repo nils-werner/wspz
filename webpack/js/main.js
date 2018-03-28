253309d4af8972c08e2aad59700df25d7d53dd0d
Nils Werner <nils.werner@audiolabs-erlangen.de>
2018-03-28 18:43:36 +0200

    AnchorJS

--- webpack/js/main.js
+++ webpack/js/main.js
@@ -1,2 +1,7 @@
 require("bootstrap");
 require("lightbox2");
+
+var anchorJS = require('anchor-js');
+var anchors = new anchorJS({'placement': 'left'});
+
+anchors.add('.text-block h2, .text-block h3');
