59047996cddce9c4fce3055779d14285914c5f05
Nils Werner <nils.werner@gmail.com>
2019-11-16 12:39:27 +0100

    Dark theme

--- webpack/js/main.js
+++ webpack/js/main.js
@@ -1,7 +1,15 @@
 require("bootstrap");
 require("lightbox2");
-
+var DarkReader = require('darkreader');
 var anchorJS = require('anchor-js');
-var anchors = new anchorJS({'placement': 'left'});
 
+var anchors = new anchorJS({'placement': 'left'});
 anchors.add('.text-block h2, .text-block h3');
+
+if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  DarkReader.enable({
+      brightness: 100,
+      contrast: 90,
+      sepia: 10,
+  });
+}
